b3525539a35a68a29521d6711ed5e380b7edfd29
aziz-jouini <ajouini060@gmail.com>
2024-04-28 13:24:38 +0100

    5ème commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  loginForm !: FormGroup;
+ 
+  constructor (private fb:FormBuilder) {}
+  ngOnInit () {
+    this.loginForm = this.fb.group({
+      
+      email:['', [Validators.email, Validators.required]],
+      password:['',[Validators.minLength(5), Validators.required]],
+      
+    })
+  }
+  login() {
+    console.log("hi",this.loginForm.value);
+  }
 }
